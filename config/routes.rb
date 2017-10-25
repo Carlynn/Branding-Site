4515d32feff9e80f05144911b19e46fc2f52b033
Carlynn <chick04sd@gmail.com>
2017-10-24 18:33:19 -0700

    edit page and home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :admin
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "hcdesigns#index"
   resources :hcdesigns
 end
