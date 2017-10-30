fb8634adcf2035bd76ecb6a1b506f232a508c0bd
Carlynn <chick04sd@gmail.com>
2017-10-30 09:35:42 -0700

    updates
    upadted s3 names

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,9 +96,9 @@ Rails.application.configure do
        s3_credentials: {
          access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
          secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
-         s3_host_name: ENV['s3-us-west-1.amazonaws.com'],
+         s3_host_name: ENV.fetch('S3_HOST_NAME'),
          s3_region: ENV.fetch('AWS_REGION')
        }
    }
-   
+
 end
